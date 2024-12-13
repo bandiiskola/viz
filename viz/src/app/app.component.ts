ab873cf0a85946b5580180a6a175f5d076676007
Kovács Csaba <0tac-kovcsa@sztu-02.moriczref.lan>
2024-12-13 08:47:43 +0100

    commit

--- viz/src/app/app.component.ts
+++ viz/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooldalComponent } from './fooldal/fooldal.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
